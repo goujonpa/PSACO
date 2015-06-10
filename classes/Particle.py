9623131b8112b406fd6cf8fae66a8d561b2c15e4
goujonpa <goujonpa@gmail.com>
2015-06-10 18:04:11 -0300

    basic implementation

--- classes/Particle.py
+++ classes/Particle.py
@@ -1,9 +1,70 @@
 #!/usr/local/bin/python
 # -*-coding:Utf-8 -*
 
+from modules.ackley import f as ackley_f
+
 
 class Particle(object):
-    """Particle class : Ackley individual representation in PSA algorithm"""
+    """Particle class : Ackley individual representation in PSACO algorithm"""
 
-    def __init__(self):
+    def __init__(
+        self,
+        position=None,
+        velocity=None,
+        best_position=None,
+        best_fitness=None,
+        c1=None,
+        c2=None,
+        c3=None,
+    ):
+        self._position = position
+        self._velocity = velocity
+        self._best_position = best_position
+        self._best_fitness = best_fitness
+        self._c1 = c1
+        self._c2 = c2
+        self._c3 = c3
         super(Particle, self).__init__()
+
+    @property
+    def position(self):
+        return self._position
+
+    @property
+    def velocity(self):
+        return self._velocity
+
+    @property
+    def best_position(self):
+        return self._best_position
+
+    @property
+    def best_fitness(self):
+        return self._best_fitness
+
+    @property
+    def c1(self):
+        return self._c1
+
+    @property
+    def c2(self):
+        return self._c2
+
+    @property
+    def c3(self):
+        return self._c3
+
+    @property
+    def dimension(self):
+        return len(self.position)
+
+    def get_fitness(self):
+        ackley_result = ackley_f(
+            c1=self.c1,
+            c2=self.c2,
+            c3=self.c3,
+            dimension=self.dimension,
+            position=self.position
+        )
+        fitness = 1.0 / (1.0 + ackley_result)
+        return fitness
--- classes/Population.py
+++ classes/Population.py
@@ -5,6 +5,37 @@
 class Population(object):
     """Population class : contains the Ackley individual represented by Particles"""
 
-    def __init__(self, arg):
+    def __init__(
+        self,
+        population=None,
+        size=None,
+        max_iter=None,
+        max_fitness=None
+        best_global_pos=None
+        best_global_fit=None
+        max_inertia=None
+        min_inertia=None
+        max_velocity=None
+        min_velocity=None
+        cognitive_scaling=None
+        social_scaling=None
+        min_sd=None
+        current_sd=None
+        sd_dec=None
+    ):
+        self._population = population
+        self._size = size
+        self._max_iter = max_iter
+        self._max_fitness = max_fitness
+        self._best_global_pos = best_global_pos
+        self._best_global_fit = best_global_fit
+        self._max_inertia = max_inertia
+        self._min_inertia = min_inertia
+        self._max_velocity = max_velocity
+        self._min_velocity = min_velocity
+        self._cognitive_scaling = cognitive_scaling
+        self._social_scaling = social_scaling
+        self._min_sd = min_sd
+        self._current_sd = current_sd
+        self._sd_dec = sd_dec
         super(Population, self).__init__()
-        self.arg = arg
